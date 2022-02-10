7d8f0ed209886312a1f3038947fc3c7991320a4a
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-02-10 17:55:11 -0300

    Valida negociacao em dia uteis

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -3,6 +3,7 @@ import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 import { MensagemView } from '../views/mensagem-view.js'
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -21,9 +22,21 @@ export class NegociacaoController {
 
     public adiciona(): void {        
         const negociacao = this.criaNegociacao();
+
+        if( ! this.ehDiaUtil(negociacao.data))
+        {
+            this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
+            return ;
+        }
+
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+    
+    private ehDiaUtil(data:Date)
+    {
+        return  data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
     }
 
     private criaNegociacao(): Negociacao {
