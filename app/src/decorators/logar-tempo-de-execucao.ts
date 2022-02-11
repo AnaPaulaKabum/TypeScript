554a9c2507304a9beb4deda6034a7aebc1777f3f
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-02-11 13:35:11 -0300

    Possibilita o log em seg ou mm

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao()
+export function logarTempoDeExecucao(emSegundos:boolean = false)
 {
     return function (target:any, propertyKey:string,
                     descriptor: PropertyDescriptor)
@@ -8,13 +8,21 @@ export function logarTempoDeExecucao()
 
         descriptor.value = function(...args: Array<any>)
         {
+            let divisor = 1;
+            let unidade = 'milisegundos';
+
+            if (emSegundos)
+            {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
             const t1 = performance.now();
 
             //chamar metodo original
             const retorno = metodoOriginal.apply(this,args);
 
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000} segundos `);
+            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/divisor} ${unidade} `);
             //retorno;
         }
         return descriptor;
