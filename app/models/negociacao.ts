e555e3b2fa80319464de663ae85e4d9e5ffc1557
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-02-11 09:17:59 -0300

    Metodo estatico na classe Negociacao

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -17,4 +17,14 @@ export class Negociacao{
         return data;
     }
 
+    public static criaDe(dataString: string, quantidadeString:string, valorString:string)
+    {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+
 }
