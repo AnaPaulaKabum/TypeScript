cf6ef348e23e36bf5c563d9f01cf31d529cf2ff1
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-02-11 13:54:05 -0300

    Criado decorator para inspecionar funcoes

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 
 export abstract class View <T>{
@@ -21,6 +22,7 @@ export abstract class View <T>{
         }
     };
 
+    @inspect()
     @logarTempoDeExecucao(true)
     public update(model: T):void{
 
